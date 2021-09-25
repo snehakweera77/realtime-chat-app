9be6abf9be76b5504fcdfbaf36fe4548a501029d
snehakweera77 <snehakweera77@gmail.com>
2021-09-25 23:23:14 +0530

    Main activity updated

--- app/src/main/java/com/example/realtimechatapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/realtimechatapp/activity/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.realtimechatapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.amplifyframework.auth.AuthUser
+import com.amplifyframework.auth.AuthUserAttributeKey
+import com.amplifyframework.auth.options.AuthSignUpOptions
+import com.amplifyframework.core.Amplify
 import com.example.realtimechatapp.R
 import com.example.realtimechatapp.adapter.ChatRoomItemAdapter
 import com.example.realtimechatapp.model.ChatRoomItem
@@ -12,39 +18,16 @@ import kotlinx.android.synthetic.main.chat_room_item.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val currentUser = Amplify.Auth.currentUser
+        val intent: Intent
+        if (currentUser == null) {
+            intent = Intent(applicationContext, LoginActivity::class.java)
+        }
+        else {
+            intent = Intent(applicationContext, ChatRoomActivity::class.java)
 
-        chatRV.setHasFixedSize(true)
-        chatRV.setItemViewCacheSize(13)
-        chatRV.layoutManager = LinearLayoutManager(this)
-        val userList = ArrayList<ChatRoomItem>()
-        userList.add(
-            ChatRoomItem("1",
-                "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg", "Vadim", "2020-10-03T14:48:00.000Z",
-                "btw, SpaceX is interested in buying notJust.dev!", "1")
-        )
-        userList.add(
-            ChatRoomItem("1",
-                "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg", "Vadim", "2020-10-03T14:48:00.000Z",
-                "btw, SpaceX is interested in buying notJust.dev!")
-        )
-        userList.add(
-            ChatRoomItem("1",
-                "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg", "Vadim", "2020-10-03T14:48:00.000Z",
-                "btw, SpaceX is interested in buying notJust.dev!", "3")
-        )
-        userList.add(
-            ChatRoomItem("1",
-                "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg", "Vadim", "2020-10-03T14:48:00.000Z",
-                "btw, SpaceX is interested in buying notJust.dev!", "3")
-        )
-        userList.add(
-            ChatRoomItem("1",
-                "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg", "Vadim", "2020-10-03T14:48:00.000Z",
-                "btw, SpaceX is interested in buying notJust.dev!", "3")
-        )
-        val userAdapter = ChatRoomItemAdapter(this@MainActivity, userList)
-        chatRV.adapter = userAdapter
-
+        }
+        startActivity(intent)
+        finish()
     }
 }
